883544a80702ec7b08d52493a3cb8119e708be58
ПК <slava_zen@mail.ru>
2026-03-03 23:35:02 +0500

    указал scanBasePackages

--- src/main/java/com/order/furniture/billboards/order_furniture_billboards/OrderFurnitureBillboardsApplication.java
+++ src/main/java/com/order/furniture/billboards/order_furniture_billboards/OrderFurnitureBillboardsApplication.java
@@ -3,7 +3,7 @@ package com.order.furniture.billboards.order_furniture_billboards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.order.furniture.billboards")
 public class OrderFurnitureBillboardsApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/order/furniture/billboards/order_furniture_billboards/OrderFurnitureBillboardsApplicationTests.java
+++ src/test/java/com/order/furniture/billboards/order_furniture_billboards/OrderFurnitureBillboardsApplicationTests.java
@@ -3,7 +3,7 @@ package com.order.furniture.billboards.order_furniture_billboards;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = OrderFurnitureBillboardsApplication.class)
 class OrderFurnitureBillboardsApplicationTests {
 
     @Test
